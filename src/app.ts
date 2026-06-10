fded9f4ef8d7a74bf2496abebb4bdbf6ceed6da6
Manu <manu.medna@gmail.com>
2026-06-10 17:55:53 -0300

    mensaje

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,18 @@
-console.log("Hello");
+import express from "express";
+
+export class App {
+  public readonly app;
+
+  constructor() {
+    this.app = express();
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  public start() {
+    this.app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+    console.log("App started");
+  }
+}
